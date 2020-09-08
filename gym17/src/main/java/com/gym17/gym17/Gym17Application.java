fc607ba03e08868c1d8049e1b392d1fffd2649da
Matko Music <matko@loyalzoo.com>
2020-09-08 15:58:15 +0200

    test
    	#

---
+++ gym17/src/main/java/com/gym17/gym17/Gym17Application.java
@@ -0,0 +1,13 @@
+package com.gym17.gym17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gym17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gym17Application.class, args);
+	}
+
+}
---
+++ gym17/src/test/java/com/gym17/gym17/Gym17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym17.gym17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gym17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
