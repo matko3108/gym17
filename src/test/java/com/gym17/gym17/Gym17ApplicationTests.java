2047e0146f115156854d4f5120580e4453b131bb
Matko Music <matko@loyalzoo.com>
2020-09-17 12:44:00 +0200

    changed java version

--- src/test/java/com/gym17/gym17/Gym17ApplicationTests.java
+++ src/test/java/com/gym17/gym17/Gym17ApplicationTests.java
@@ -1,13 +1,12 @@
-package src.test.java.com.gym17.gym17;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Gym17ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package src.test.java.com.gym17.gym17;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class Gym17ApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
