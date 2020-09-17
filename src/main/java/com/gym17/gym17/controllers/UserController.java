e4beff26bfa8fe2aeb6078f18f56affc62133844
Matko Music <matko@loyalzoo.com>
2020-09-17 13:11:40 +0200

    Changed pom

--- src/main/java/com/gym17/gym17/controllers/UserController.java
+++ src/main/java/com/gym17/gym17/controllers/UserController.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
