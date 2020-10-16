90a80fed0dc930bf6076479de248c13f70af38dc
Matko Music <matko@loyalzoo.com>
2020-10-16 18:11:19 +0200

    added web page

--- src/main/java/com/gym17/gym17/MyConfiguration.java
+++ src/main/java/com/gym17/gym17/MyConfiguration.java
@@ -3,11 +3,12 @@ package src.main.java.com.gym17.gym17;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MyConfiguration {
+public class MyConfiguration implements WebMvcConfigurer{
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -18,5 +19,12 @@ public class MyConfiguration {
                         .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
             }
         };
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/**")
+          .addResourceLocations("/resources/static/");	
     }
 }
